e4f0bcaafc5d76dac7b2703a035e80fd54c22049
anatoly techtonik <techtonik@gmail.com>
2018-06-28 21:42:18 +0700

    Add Ctrl+B key binding for command line prompt

--- notfar.py
+++ notfar.py
@@ -2,7 +2,16 @@
 
 import os
 
+# requires prompt-toolkit > 2
 from prompt_toolkit import prompt
+from prompt_toolkit.key_binding import KeyBindings
 
-text = prompt(os.getcwd() + ">")
+
+global_kb = KeyBindings()
+
+@global_kb.add('c-b')  # Ctrl+B
+def switch_toolbar(event):
+    print(event)
+
+text = prompt(os.getcwd() + ">", key_bindings=global_kb)
 print("[debug] " + text)
