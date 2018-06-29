618d802889161eef44f539ef7c95d9086d76596f
anatoly techtonik <techtonik@gmail.com>
2018-06-29 08:33:52 +0700

    Add bottom toolbar to Ctrl+B key binding

--- notfar.py
+++ notfar.py
@@ -3,15 +3,29 @@
 import os
 
 # requires prompt-toolkit > 2
-from prompt_toolkit import prompt
+from prompt_toolkit import PromptSession
 from prompt_toolkit.key_binding import KeyBindings
 
 
 global_kb = KeyBindings()
 
+# if bottom toolbar is visible
+global_toolbar = None
+
+def bottom_toolbar():
+    return "F1 Help"
+
 @global_kb.add('c-b')  # Ctrl+B
 def switch_toolbar(event):
-    print(event)
+    global global_toolbar, session
+    if not global_toolbar:
+        global_toolbar = bottom_toolbar
+    else:
+        global_toolbar = None
+    session.bottom_toolbar = global_toolbar
 
-text = prompt(os.getcwd() + ">", key_bindings=global_kb)
+
+session = PromptSession(os.getcwd() + ">", key_bindings=global_kb, bottom_toolbar=global_toolbar)
+text = session.prompt()
 print("[debug] " + text)
+
